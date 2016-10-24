4f337bf5cb3f686cc5dea50fa7605af6c83e047e
David-Lundmark <david.lundmarkbredband.net>
2016-10-24 10:02:37 +0200

    new class Person

---
+++ HelloWorldGitHub/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldGitHub
+{
+    class Person
+    {
+        // Belongs to Feature 1.1
+    }
+}
